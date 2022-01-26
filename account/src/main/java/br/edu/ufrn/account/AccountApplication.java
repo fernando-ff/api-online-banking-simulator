87c26a3a20094407b5aac0490186a1e7f6221b5e
matheusmota3242 <matheusmota3242@gmail.com>
2022-01-26 17:53:52 -0300

    concluindo adaptacado o ms de course p/ account

--- account/src/main/java/br/edu/ufrn/account/AccountApplication.java
+++ account/src/main/java/br/edu/ufrn/account/AccountApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import br.edu.ufrn.core.property.JwtConfiguration;
 
 @SpringBootApplication
-@EntityScan({"br.edu.ufrn.account.model"})
+@EntityScan({"br.edu.ufrn.account.model", "br.edu.ufrn.core.model"})
 @EnableJpaRepositories({"br.edu.ufrn.core.repository", "br.edu.ufrn.account.dao"})
 @EnableConfigurationProperties(value = JwtConfiguration.class)
 @ComponentScan("br.edu.ufrn")
