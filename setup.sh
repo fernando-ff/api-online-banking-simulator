105c978af3dbe08ec3beab954b8da410e6b9ab98
Márcio Tenório <marciotenorioj@gmail.com>
2022-01-29 11:14:20 -0300

    Aviso para ainda rodar o comando de execução do .jar

--- setup.sh
+++ setup.sh
@@ -7,16 +7,17 @@ mvn clean install -DskipTests
 cd ..
 cd discovery
 mvn clean install -DskipTests
-#java -jar target/discovery.jar
+# gnome-terminal -x sh -c "java -jar target/discovery.jar"
 cd ..
 cd gateway
 mvn clean install -Dskiptests
-#java -jar target/gateway.jar
+#gnome-terminal -x sh -c "java -jar target/gateway.jar"
 cd ..
 cd auth
 mvn clean install -DskipTests
-#java -jar target/auth.jar
+#gnome-terminal -x sh -c "java -jar target/auth.jar"
 cd ..
 cd recipient 
 mvn clean install -DskipTests
-#java -jar target/recipient.jar
+#gnome-terminal -x sh -c "java -jar target/recipient.jar"
+echo You need to run: java -jar target/jarName.jar on the folders
